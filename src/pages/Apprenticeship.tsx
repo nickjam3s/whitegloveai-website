aca573231b7bac873ee6b90b0266c777a9f134fb
Jason Hess <hesscan@gmail.com>
2025-03-25 18:39:23 -0500

    removed investment section

--- src/pages/Apprenticeship.tsx
+++ src/pages/Apprenticeship.tsx
@@ -25,7 +25,6 @@ const Apprenticeship = () => {
         <CandidateProfile />
         <ApplicationProcess />
         <ContactSection />
-        <TuitionSection />
         <FAQSection />
       </PageWrapper>
     </div>
