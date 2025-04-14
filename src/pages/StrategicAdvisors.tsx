9508e67ebf7d749636894046847a5ae0b9d2a499
Jason Hess <hesscan@gmail.com>
2025-04-13 20:06:08 -0500

    updated to advisors

--- src/pages/StrategicAdvisors.tsx
+++ src/pages/StrategicAdvisors.tsx
@@ -15,36 +15,19 @@ interface AdvisorProps {
 
 const advisorData: AdvisorProps[] = [
   { name: 'Prakash Adi', title: 'CEO', company: 'Stealth Mode' },
-  { name: 'Jeff Bankard', title: 'Dir of InfoSec', company: 'Baker Tilly' },
-  { name: 'Joe Bernik', title: 'Cyber Executive', company: 'BNY Mellon' },
-  { name: 'Kristoffer Brown', title: 'Director', company: 'Mongo DB' },
   { name: 'Bindu Chowritmootoo', title: 'CEO', company: 'Manifest with Bindu' },
-  { name: 'Dre Graves', title: 'Senior Account Executive', company: 'CoreLogic' },
   { name: 'Josh Hernandez', title: 'President', company: 'Innuvis' },
   { name: 'Reggie Hillard', title: 'CEO', company: 'TeleTechTX' },
   { name: 'Young Han', title: 'Partner', company: 'Forever Young Agency' },
   { name: 'Ken Huang', title: 'CEO & CAIO', company: 'DistributedApps.ai' },
-  { name: 'Jason James', title: 'Director of Security', company: 'Mirantis' },
-  { name: 'Michael Jones', title: 'President', company: 'B1 Bank' },
-  { name: 'Paul Kanneman', title: 'Former CIO', company: 'Simmons Bank' },
-  { name: 'Rehan Kauser', title: 'CEO and Founder', company: 'Agile Advantages' },
   { name: 'Steven Kazan', title: 'CEO', company: 'Inner Onion' },
-  { name: 'Chris Lee', title: 'CIO', company: 'City of Lewisville' },
   { name: 'Lloyd Nelson', title: 'Vice President', company: 'Lockton Dunning' },
-  { name: 'Tommy Perniciaro', title: 'Vice President', company: 'Halcyon' },
-  { name: 'Randy Potts', title: 'CISO', company: 'Realtime Recovery' },
-  { name: 'Sameer Ranjan', title: 'CEO', company: 'Catenate' },
   { name: 'Rodriguez Richard', title: 'CEO', company: 'ClearSync' },
   { name: 'Michelle Rider', title: 'CMO', company: 'Endeavor Management' },
-  { name: 'Ted Sanders', title: 'BISO', company: 'Bank of America' },
-  { name: 'Binni Skariah', title: 'CEO', company: 'Interaxion' },
   { name: 'Jill Stelfox', title: 'CEO', company: 'FounderGro' },
   { name: 'Ichan Stall', title: 'Partner', company: 'Forever Young Agency' },
   { name: 'Tim Tatarowicz', title: 'Co-Founder', company: 'Phoenix Power Supply' },
   { name: 'Rick Thompson', title: 'Head of McKinney', company: 'Plug and Play' },
-  { name: 'Matt Ussery', title: 'CIO', company: 'AustenTell' },
-  { name: 'Richard Weiss', title: 'CISO', company: 'MRO' },
-  { name: 'Clement Yu', title: 'VP of Engineering', company: 'LocaliQ' }
 ];
 
 const AdvisorCard = ({ name, title, company }: AdvisorProps) => {
